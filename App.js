5012178a947e468cf802752105f5682aa51c5a11
forseasummer <summerrohh@gmail.com>
2023-06-08 19:45:39 +1000

    from lg laptop

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
-import Loading from './Loading';
-import Chicken from './Chicken';6
+import React, { useState } from "react";
+import Loading from "./Loading";
+import Chicken from "./Chicken";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
-  if ( isLoading ) {
-  return <Loading />
-  }
-
-  else{
-    return <Chicken />
+  if (isLoading) {
+    return <Loading />;
+  } else {
+    return <Chicken />;
   }
 }
 
